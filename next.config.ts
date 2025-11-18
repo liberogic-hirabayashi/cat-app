6744607dfee390400a746e3190c7af6030d2a488
平林真奈 <hirabayashi@liberogic.jp>
2025-11-18 17:25:00 +0900

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn2.thecatapi.com',
+          pathname: '/**', // すべてのパスを許可
+        },
+      
+      ],
+    },
 };
 
 export default nextConfig;
